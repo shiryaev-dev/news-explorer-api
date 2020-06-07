dcc608d6ff755a8b99f774eaed01860dfbb94c42
Alexey Shiryaev <ashiryaev67@yandex.ru>
2020-06-08 02:47:55 +0300

    change port

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 const prod = process.env.NODE_ENV === 'production';
 module.exports = {
   JWT_SECRET: prod ? process.env.JWT_SECRET : 'secret key',
-  PORT: 3000 || process.env.PORT,
+  PORT: process.env.PORT || 3000,
   MONGO_URI: 'mongodb://localhost:27017/diplomadb',
 };
